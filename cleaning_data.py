41ebc679c402aefc2f49a3f54a0065e2886beda0
Yoonku Hwang <yz0624@gmail.com>
2015-11-18 22:20:27 +0900

    add category avg column

--- cleaning_data.py
+++ cleaning_data.py
@@ -1,5 +1,14 @@
-from pandas import DataFrame, read_csv
-df = read_csv('baseball_data.csv')
-df = df[df.avg > 0.0]
+from pandas import DataFrame, read_csv, cut
+# import matplotlib.pyplot as plt
 
-print df.to_csv('cleaned_baseball_data.csv')
+df = read_csv('data/baseball_data.csv')
+print df.describe()
+
+df = df[(df.avg > 0.0) & (df.HR > 0)]
+print df.describe()
+
+print df.groupby(by='handedness').describe()
+
+import numpy as np
+df['avg_category'] = cut(df.avg, np.linspace(0.0, 0.35, 8))
+print df
